b4caf0d2d675011e7a89b37c091762e955e26f9c
MrHarte <111158364+MrHarte@users.noreply.github.com>
2026-09-09 17:30:38 -0400

    Initial project version with view created.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.champlain.oop2.oop2a1f26 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.champlain.oop2.oop2a1f26 to javafx.fxml;
+    exports org.champlain.oop2.oop2a1f26;
+}
---
+++ src/main/java/org/champlain/oop2/oop2a1f26/PersonViewApplication.java
@@ -0,0 +1,19 @@
+package org.champlain.oop2.oop2a1f26;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PersonViewApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(PersonViewApplication.class.getResource("person-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 350, 350);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
